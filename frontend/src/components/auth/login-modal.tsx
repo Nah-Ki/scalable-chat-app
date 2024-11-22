61e8752a730566d7e0b41f7ee15035df3cd9fdcf
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-11-22 19:50:37 +0530

    fix: merge conflicts

--- frontend/src/components/auth/login-modal.tsx
+++ frontend/src/components/auth/login-modal.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-<<<<<<< HEAD
-=======
 import { signIn } from "next-auth/react";
->>>>>>> 6774ddd2ef6061bdefa514a108842bcaa67046da
 import Image from "next/image";
 import { Button } from "../ui/button";
 
@@ -25,11 +22,7 @@ export default function LoginModal() {
         <DialogHeader>
           <DialogTitle className="text-2xl mx-auto">Login</DialogTitle>
         </DialogHeader>
-<<<<<<< HEAD
-        <Button variant="outline" className="mx-auto">
-=======
         <Button variant="outline" onClick={handleLogin}>
->>>>>>> 6774ddd2ef6061bdefa514a108842bcaa67046da
           <Image src="/images/google.png" className="mr-4" width={25} height={25} alt="google_log"></Image>
         </Button>
       </DialogContent>
